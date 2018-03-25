9137ee31cd330e0cc9698cd174eb0a5ed0d0d024
sooryagangarajk <sooryagangaraj@gmail.com>
2018-03-25 12:53:05 +0530

    initial commit

---
+++ Bot.js
@@ -0,0 +1,16 @@
+console.log('News bot started...');
+var GoogleNews, googleNews, track;
+  
+GoogleNews = require('google-news');
+googleNews = new GoogleNews();
+track = 'Volvo';
+googleNews.stream(track, function(stream) {
+  
+  stream.on(GoogleNews.DATA, function(data) {
+    return console.log('Data Event received... ' + data.title);
+  });
+  
+  stream.on(GoogleNews.ERROR, function(error) {
+    return console.log('Error Event received... ' + error);
+  });
+});
